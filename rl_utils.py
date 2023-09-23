b19c90d0caccd8aa9c6ddb066dd0a093d1435fb9
carlfredriksson <c@msp.se>
2023-09-23 23:37:25 +0200

    Worked on CartPole

--- rl_utils.py
+++ rl_utils.py
@@ -1,6 +1,9 @@
 from collections import deque
 import random
+import numpy as np
 
+# For big projects, consider using torchrl.data.ReplayBuffer.
+# For more info, see https://pytorch.org/rl/tutorials/rb_tutorial.html.
 class ReplayMemory():
     def __init__(self, size):
         self._memory = deque(maxlen=size)
@@ -11,10 +14,9 @@ class ReplayMemory():
     def store(self, state, action, reward, next_state):
         self._memory.append((state, action, reward, next_state))
 
-    def retrieve_sample_batch(self, batch_size):
-        sample = random.sample(self._memory, batch_size)
+    def sample_batch(self, batch_size):
+        batch = random.sample(self._memory, batch_size)
         # Transpose list of (state, action, reward, next_state) to
         # (state_batch, action_batch, reward_batch, next_state_batch). For more info, see
-        # https://stackoverflow.com/questions/19339/transpose-unzip-function-inverse-of-zip/19343#19343
-        sample_transposed = zip(*sample)
-        return map(np.array, sample_transposed)
+        # https://stackoverflow.com/questions/19339/transpose-unzip-function-inverse-of-zip/19343#19343.
+        return zip(*batch)
