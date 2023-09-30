37f71d03076119a8ee4bfbf6cf9af2805420ea0f
carlfredriksson <c@msp.se>
2023-09-30 15:13:47 +0200

    Worked on CartPole

--- models.py
+++ models.py
@@ -1,23 +1,10 @@
 from torch import nn
 
-class LinearModel(nn.Module):
-    def __init__(self, num_input, num_output):
-        super().__init__()
-        self.linear = nn.Linear(num_input, num_output)
-    
-    def forward(self, X):
-        return self.linear(X)
-
-class SimpleNN(nn.Module):
-    def __init__(self, num_input_dims, num_output_dims, num_hidden_layer_dims):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(num_input_dims, num_hidden_layer_dims),
-            nn.ReLU(),
-            nn.Linear(num_hidden_layer_dims, num_hidden_layer_dims),
-            nn.ReLU(),
-            nn.Linear(num_hidden_layer_dims, num_output_dims)
-        )
-    
-    def forward(self, X):
-        return self.layers(X)
+def create_simple_nn(input_size, output_size, hidden_layer_sizes):
+    layer_sizes = [input_size] + hidden_layer_sizes + [output_size]
+    layers = []
+    for i in range(len(layer_sizes)-1):
+        layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+        if i < len(layer_sizes)-2:
+            layers.append(nn.ReLU())
+    return nn.Sequential(*layers)
