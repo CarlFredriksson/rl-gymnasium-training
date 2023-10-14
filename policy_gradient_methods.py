baec35e7bda6cf23fb338c4d2f1ebb083aed4a24
carlfredriksson <c@msp.se>
2023-10-15 00:54:16 +0200

    Worked on CartPole

--- policy_gradient_methods.py
+++ policy_gradient_methods.py
@@ -49,3 +49,62 @@ def train_episodic_reinforce(env, policy_model, optimizer, device, rng_seed, num
         optimizer.step()
         
     return returns, states, actions, rewards
+
+def train_episodic_reinforce_with_baseline(
+        env, policy_model, value_model, policy_optimizer, value_optimizer, device, rng_seed, num_episodes, gamma, grad_clip_value=None):
+    returns = []
+    for episode in range(num_episodes):
+        # Initiate episode
+        # Set seed only one time per training run. For more info, see https://gymnasium.farama.org/api/env/.
+        seed = rng_seed if episode == 0 else None
+        observation, info = env.reset(seed=seed)
+        state = torch.tensor(observation, device=device)
+        truncated = False
+        terminated = False
+        G = 0
+        states = [state]
+        actions = []
+        rewards = []
+
+        # Generate episode
+        while not (terminated or truncated):
+            action = select_action_softmax(policy_model, state)
+            observation, reward, terminated, truncated, info = env.step(action)
+            state = torch.tensor(observation, device=device)
+            states.append(state)
+            actions.append(torch.tensor([action], device=device))
+            rewards.append(reward)
+            G += reward
+        returns.append(G)
+
+        # Learn from episode
+        discounted_returns = []
+        discount_factors = []
+        G = 0
+        for t in reversed(range(len(rewards))):
+            G = gamma * G + rewards[t]
+            discounted_returns.append(G)
+            discount_factors.append(gamma**t)
+        discounted_returns.reverse()
+        discounted_returns = torch.tensor(discounted_returns, device=device)
+        discount_factors.reverse()
+        discount_factors = torch.tensor(discount_factors, device=device)
+        states = torch.stack(states[:-1], dim=0)
+        actions = torch.stack(actions, dim=0)
+        with torch.no_grad():
+            state_values = value_model(states).squeeze()
+        delta = discounted_returns - state_values
+        delta = discounted_returns
+        policy_loss = -torch.sum(discount_factors * delta * torch.log(policy_model(states).gather(1, actions)).squeeze())
+        policy_optimizer.zero_grad()
+        policy_loss.backward()
+        value_loss = -torch.sum(delta * value_model(states).squeeze())
+        value_optimizer.zero_grad()
+        value_loss.backward()
+        if grad_clip_value != None:
+            torch.nn.utils.clip_grad_value_(policy_model.parameters(), grad_clip_value)
+            torch.nn.utils.clip_grad_value_(value_model.parameters(), grad_clip_value)
+        policy_optimizer.step()
+        value_optimizer.step()
+        
+    return returns, states, actions, rewards
