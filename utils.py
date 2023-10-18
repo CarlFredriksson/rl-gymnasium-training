bce78f67831598e9f596b321dd051e5640ecb704
carlfredriksson <c@msp.se>
2023-10-18 20:34:42 +0200

    Worked on CartPole

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def rolling_average(numbers_to_average, window_size):
+def compute_rolling_average(numbers_to_average, window_size):
     rolling_averages = []
     for i in range(len(numbers_to_average)):
         window_start = max(i - window_size + 1, 0)
@@ -15,7 +15,7 @@ def plot_returns(returns, target=None):
     print("Average return per episode:", np.sum(returns) / len(returns))
     episodes = np.arange(len(returns))
     plt.plot(episodes, returns, label="Return per episode")
-    plt.plot(episodes, rolling_average(returns, 100), label="Rolling average return last 100 episodes")
+    plt.plot(episodes, compute_rolling_average(returns, 100), label="Rolling average return last 100 episodes")
     if target is not None:
         plt.plot(episodes, np.ones(len(returns)) * target, label="Rolling average return \"solving\" target")
     plt.xlabel("Episode")
@@ -33,15 +33,14 @@ def plot_returns_multiple_runs(returns_per_run, target=None):
                 returns = returns_per_run[i]
                 episodes = np.arange(len(returns))
                 axs[row, col].plot(episodes, returns, label="Return per episode")
-                axs[row, col].plot(episodes, rolling_average(returns, 100), label="Rolling average return last 100 episodes")
+                axs[row, col].plot(episodes, compute_rolling_average(returns, 100), label="Rolling average return last 100 episodes")
                 if target is not None:
                     axs[row, col].plot(episodes, np.ones(len(returns)) * target, label="Rolling average return \"solving\" target")
-                axs[row, col].set_xlabel("Episode")
-                axs[row, col].set_ylabel("Return")
             else:
                 fig.delaxes(axs[row, col])
             i += 1
+    fig.supxlabel("Episode")
+    fig.supylabel("Return")
     handles, labels = axs[0, 0].get_legend_handles_labels()
-    #fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0., 0., 0., 0.))
     fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.5, 1.04))
     plt.tight_layout()
