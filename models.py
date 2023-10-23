a13208d10d640cb661a5b5a43ea63b9686dd8710
carlfredriksson <c@msp.se>
2023-10-24 00:49:54 +0200

    Worked on CartPole

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from torch import nn
 
-def create_simple_nn(input_size, output_size, hidden_layer_sizes, output_activation=None):
+def create_simple_nn(input_size, output_size, hidden_layer_sizes, output_activation=None, output_dim=None):
     layer_sizes = [input_size] + hidden_layer_sizes + [output_size]
     layers = []
     for i in range(len(layer_sizes)-1):
@@ -8,5 +8,5 @@ def create_simple_nn(input_size, output_size, hidden_layer_sizes, output_activat
         if i < len(layer_sizes)-2:
             layers.append(nn.ReLU())
     if output_activation == "softmax":
-        layers.append(nn.Softmax(dim=1))
+        layers.append(nn.Softmax(dim=1 if output_dim is None else output_dim))
     return nn.Sequential(*layers)
